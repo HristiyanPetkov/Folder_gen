6be9ddaacea2673a7529206ba1a9dbe47cb0a775
Hrisitqn Petkov <71525781+HristiyanPetkov@users.noreply.github.com>
2021-04-21 14:00:53 +0300

    Update file_generator.py

--- file_generator.py
+++ file_generator.py
@@ -11,7 +11,6 @@ else:
     for i in range(num_of_folders):
         os.mkdir(f"folders/folder{i + 1}")
         random_num = random.randint(0, 10)
-        print(random_num)
         for j in range(random_num):
             with open(f"folders/folder{i + 1}/file{j + 1}.txt", 'w') as file:
                 file.write(f"{j + 1}")
